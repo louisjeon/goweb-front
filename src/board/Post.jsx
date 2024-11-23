3b2ef289047b89e0abcbd128424cced3a50e5056
louisjeon <wjss5115@gmail.com>
2024-11-23 15:55:54 +0900

    add editpost

--- src/board/Post.jsx
+++ src/board/Post.jsx
@@ -1,5 +1,4 @@
 import styled from "styled-components";
-import PostElement from "../board/PostElement";
 import { useEffect, useState } from "react";
 import { useSearchParams } from "react-router-dom";
 
@@ -25,31 +24,62 @@ const Post = () => {
   const [editMode, setEditMode] = useState(false);
   const [title, setTitle] = useState();
   const [content, setContent] = useState();
+  const [editTitle, setEditTitle] = useState();
+  const [editContent, setEditContent] = useState();
 
   useEffect(() => {
     setTitle("포스트 제목");
     setContent("포스트 내용");
+    setEditTitle(title);
+    setEditContent(content);
   }, []);
+
+  const handleEditStart = () => {
+    setEditMode(true);
+  };
+
+  const handleEditFinish = () => {
+    setTitle(editTitle);
+    setContent(editContent);
+    setEditMode(false);
+  };
+
+  const handleEditTitleChange = (e) => {
+    setEditTitle(e.target.value);
+  };
+
+  const handleEditContentChange = (e) => {
+    setEditContent(e.target.value);
+  };
 
   return (
     <StyledPost>
       <div className="boardContainer">
         {editMode ? (
-          <input class="editTitle" defaultValue={title} />
+          <input
+            class="editTitle"
+            defaultValue={title}
+            onChange={handleEditTitleChange}
+          />
         ) : (
           <h1>{title}</h1>
         )}
         <p>포스트 날짜</p>
         {editMode ? (
-          <textarea class="editContent" defaultValue={content} rows={20} />
+          <textarea
+            class="editContent"
+            defaultValue={content}
+            rows={20}
+            onChange={handleEditContentChange}
+          />
         ) : (
           <p>{content}</p>
         )}
         <p>{searchParams.get("id")}번 포스트입니다.</p>
         {editMode ? (
-          <button onClick={() => setEditMode(false)}>수정 완료</button>
+          <button onClick={handleEditFinish}>수정 완료</button>
         ) : (
-          <button onClick={() => setEditMode(true)}>포스트 수정</button>
+          <button onClick={handleEditStart}>포스트 수정</button>
         )}
       </div>
     </StyledPost>
