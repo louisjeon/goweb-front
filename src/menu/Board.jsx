43ff0fc4161a58c62802747d1eaf2156eb5e4ddd
louisjeon <wjss5115@gmail.com>
2024-12-01 01:05:38 +0900

    test

--- src/menu/Board.jsx
+++ src/menu/Board.jsx
@@ -106,6 +106,7 @@ const Board = () => {
     customAxios
       .get("/posts")
       .then((res) => {
+        console.log(res.data);
         setAllPosts(res.data.reverse());
         setPage(1);
       })
